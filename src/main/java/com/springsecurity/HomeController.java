d21e14d788bb4d4b2945b41ead713f0921be816c
Taha Arar <taha.arar@univ-constantine2.dz>
2025-06-01 17:30:38 +0100

    Rename application.properties to application.yml and update security configuration; add HomeController for welcome message

---
+++ src/main/java/com/springsecurity/HomeController.java
@@ -0,0 +1,15 @@
+package com.springsecurity;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(HttpServletRequest request) {
+        return "Welcome: "+request.getSession().getId();
+    }
+
+}
