c8700822143f9627b9f456260ba77f854d6c9a59
Taha Arar <taha.arar@univ-constantine2.dz>
2025-06-02 15:28:48 +0100

    Update JPA configuration, enhance Client entity validation, and clean dependencies.
    - Changed `ddl-auto` setting to `update` in `application.yml`.
    - Added `@EnableJpaRepositories` and `@EntityScan` annotations for repository and entity configuration.
    - Removed `spring-boot-devtools` dependency from `pom.xml`.
    - Updated `Client` entity with an explicit table name and phone number validation.

--- src/main/java/com/springsecurity/SpringSecurityApplication.java
+++ src/main/java/com/springsecurity/SpringSecurityApplication.java
@@ -2,9 +2,13 @@ package com.springsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaAuditing //  (for @CreatedDate, @LastModifiedDate to work)
+@EnableJpaRepositories(basePackages = {"com.springsecurity.repository"})
+@EntityScan(basePackages = {"com.springsecurity.model"}) // Scan for JPA entities in the specified package
 @SpringBootApplication
 public class SpringSecurityApplication {
 
--- src/main/java/com/springsecurity/model/Client.java
+++ src/main/java/com/springsecurity/model/Client.java
@@ -4,6 +4,7 @@ import com.springsecurity.model.abstractEntity.AbstractEntity;
 import jakarta.persistence.Entity;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,7 +14,7 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity
+@Entity(name = "client")
 public class Client extends AbstractEntity {
 
     @NotBlank(message = "Email is mandatory")
@@ -21,6 +22,7 @@ public class Client extends AbstractEntity {
     private String email;
 
     @NotBlank(message = "Phone number is mandatory")
+    @Size(min = 10, max = 10, message = "Phone must be exactly 10 digits")
     private String phone;
 
     private String address;
